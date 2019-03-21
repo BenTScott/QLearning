b9d8fed77f3155d11b88e05b89fea445a57d4436
Ben Scott <bentysonscott@hotmail.co.uk>
2019-03-21 14:07:49 +0000

    test

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 
 int main( int argc, const char* argv[] )
 {
-	auto r_learning = QLearning<TicTacToeState, int>(new TicTacToe(), 0.2, 1, 0.7);
+	auto r_learning = QLearning<TicTacToeState, int>(new TicTacToe(), 0.15, 1, 0.4);
 
-	auto table = r_learning.Run(5000);
+	auto table = r_learning.Run(50000);
 
 	std::cout << "Done training" << std::endl;
 
